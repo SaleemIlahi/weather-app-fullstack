fee308b4d3b547c2c7fb8ab89d1d3ddb91a53fff
Mohamed Saleem <saleemilahi21@gmail.com>
2026-01-16 16:38:22 +0530

    feat: Add CORS support via CORSMiddleware - Enable Cross-Origin Resource Sharing for the frontend origin
    - Configure allowed methods and headers

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from .api.weather import router
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
+
+frontend_origin = os.getenv("CORS_ORIGIN").split(",")
+allowed_methods = os.getenv("CORS_METHODS").split(",")
+cors_header = os.getenv("CORS_HEADERS").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=frontend_origin,
+    allow_methods=allowed_methods,
+    allow_headers=cors_header,
+)
 
 app.include_router(router, prefix="/api/v1", tags=["weather"])
